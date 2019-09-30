fc7cda20a8552092abbd5591167fa8bfa2a7d51a
jiangyu <jiangyuwise@gmail.com>
2019-09-30 16:41:14 +0800

    使用 TDD 的开发模式

---
+++ src/main/java/com/codve/TicTacToe.java
@@ -0,0 +1,76 @@
+package com.codve;
+
+public class TicTacToe {
+
+    private static final int SIZE = 3;
+
+    private Character[][] board = {
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'}
+    };
+
+    private char lastPlayer = '\0';
+
+    public String play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+        if (isWin(x, y)) {
+            return lastPlayer + " is the winner";
+        } else if (isDraw()) {
+            return "The result is draw";
+        } else {
+            return "No winner";
+        }
+    }
+
+    private boolean isWin(int x, int y) {
+        int playerTotal = lastPlayer * 3;
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal = vertical = diagonal1 = diagonal2 = 0;
+        for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][SIZE - i - 1];
+        }
+        return horizontal == playerTotal ||
+                vertical == playerTotal ||
+                diagonal1 == playerTotal ||
+                diagonal2 == playerTotal ;
+    }
+
+    private boolean isDraw() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void checkAxis(int axis) {
+        if (axis < 1 || axis > 3) {
+            throw new RuntimeException("axis is outside board.");
+        }
+    }
+
+    private void setBox(int x, int y, char lastPlayer) {
+        if (board[x - 1][y - 1] != '\0') {
+            throw new RuntimeException("box is occupied.");
+        } else {
+            board[x - 1][y - 1] = lastPlayer;
+        }
+    }
+
+    public char nextPlayer() {
+        if (lastPlayer == 'X') {
+            return 'O';
+        }
+        return 'X';
+    }
+}
---
+++ src/test/java/com/codve/TicTacToeSpec.java
@@ -0,0 +1,165 @@
+package com.codve;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicTacToeSpec {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    /**
+     * 如果棋子超过 x 轴的边界, 将引发 RuntimeException
+     */
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
+    /**
+     * 如果棋子超过 y 轴的边界, 将引发 RuntimeException
+     */
+    @Test
+    public void whenYOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
+    /**
+     * 如果棋子 x 轴是负数, 将引发 RuntimeException
+     */
+    @Test
+    public void whenXIsNegativeThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(-1, 0);
+    }
+
+    /**
+     * 如果棋子放的地方已有棋子, 将引发 RuntimeException
+     */
+    @Test
+    public void whenOccupiedThenRuntimeException() {
+        ticTacToe.play(2, 1);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 1);
+    }
+
+    /**
+     * 玩家 X 先下
+     */
+    @Test
+    public void givenFirstTurnWhenNextPlayerThenX() {
+        assertEquals('X', ticTacToe.nextPlayer());
+    }
+
+    /**
+     * 如果第一次是玩家 X 下的, 接下来应轮到玩家 O
+     */
+    @Test
+    public void givenLastTurnWasXWhenNextPlayerThenO() {
+        ticTacToe.play(1, 1);
+        assertEquals('O', ticTacToe.nextPlayer());
+    }
+
+    /**
+     * 如果最后一次是玩家 Y 下的, 接下来应轮到玩家 X
+     * 这个测试不用修改实现就能通过, 应当删除
+     */
+    @Ignore("没有用处, 应当被删除.")
+    @Test
+    public void givenLastTurnWasOWhenNextPlayerThenX() {
+        ticTacToe.play(1,1);
+        ticTacToe.play(2, 1);
+        assertEquals('X', ticTacToe.nextPlayer());
+    }
+
+    /**
+     * 无人获胜
+     */
+    @Test
+    public void whenPlayThenNoWinner() {
+        String actual = ticTacToe.play(1, 1);
+        assertEquals("No winner", actual);
+    }
+
+    @Test
+    /**
+     * 一个玩家占据整条水平线就获胜
+     */
+    public void whenPlayAndWholeHorizontalLineThenWinner() {
+        ticTacToe.play(1, 1); // x
+        ticTacToe.play(1, 2); // o
+        ticTacToe.play(2, 1); // x
+        ticTacToe.play(2, 2); // y
+        String actual = ticTacToe.play(3, 1); // x
+        assertEquals("X is the winner", actual);
+    }
+
+    /**
+     * 一个玩家占据整条垂直线就获胜
+     */
+    @Test
+    public void whenPlayAndWholeVerticalLineThenWinner() {
+        ticTacToe.play(1, 1); // x
+        ticTacToe.play(2, 1); // o
+        ticTacToe.play(1, 2); // x
+        ticTacToe.play(2, 2); // o
+        String actual = ticTacToe.play(1, 3); // x
+        assertEquals("X is the winner", actual);
+    }
+
+    /**
+     * 一个玩家占据从左上到右下的斜对角线就获胜
+     */
+    @Test
+    public void whenPlayAndTopBottomDiagonalLineThenWinner() {
+        ticTacToe.play(1, 1); // x
+        ticTacToe.play(2, 1); // y
+        ticTacToe.play(2, 2); // x
+        ticTacToe.play(1, 2); // y
+        String actual = ticTacToe.play(3, 3); // x
+        assertEquals("X is the winner", actual);
+    }
+
+    /**
+     * 一个玩家占据从左下到右上的斜对角线就获胜
+     */
+    @Test
+    public void whenPlayAndBottomTopDiagonalLineThenWinner() {
+        ticTacToe.play(1, 3);
+        ticTacToe.play(1, 1);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(2, 3);
+        String actual = ticTacToe.play(3, 1);
+        assertEquals("X is the winner", actual);
+    }
+
+    /**
+     * 格子都被填满了, 平局
+     */
+    @Test
+    public void whenAllBoxesFilledThenDraw() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(1, 3);
+        ticTacToe.play(2, 1);
+        ticTacToe.play(2, 3);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(3, 1);
+        ticTacToe.play(3, 3);
+        String actual = ticTacToe.play(3, 2);
+        assertEquals("The result is draw", actual);
+    }
+}
